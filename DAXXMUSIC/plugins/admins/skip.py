004ae97de3eeca91fbb986b6ffcf67595cd647bf
Mehmet676767 <162468939+Mehmet676767@users.noreply.github.com>
2024-03-29 22:54:20 +0300

    Update skip.py

--- DAXXMUSIC/plugins/admins/skip.py
+++ DAXXMUSIC/plugins/admins/skip.py
@@ -14,7 +14,7 @@ from config import BANNED_USERS
 
 
 @app.on_message(
-    filters.command(["skip", "cskip", "atla", "begenmedim", "cnext"]) & filters.group & ~BANNED_USERS
+    filters.command(["skip", "cskip", "atla", "cnext"]) & filters.group & ~BANNED_USERS
 )
 @AdminRightsCheck
 async def skip(cli, message: Message, _, chat_id):
